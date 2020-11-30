0a20e5868fe23fd88db6bfcb4df2c9a6e4ae7561
Gaston Sabugo <gastons_92@hotmail.com>
2020-11-30 17:05:05 -0300

    prototipo

---
+++ prototipo/myscript.js
@@ -0,0 +1,63 @@
+$(document).ready(function(){
+    $("#estilo1").click(function(){
+        //hacer  consulta Ajax
+        var consulta = "http://localhost:4567/getNoticias1";
+        $("#consulta").html("la consulta es:  "+consulta);
+        /*
+        $.get(consulta, function(answer){
+            //insertar codigo obtenido
+            console.log("consulta recibida");
+            $("#contenedorimagen").html(answer);
+        });
+        */
+        
+        $.ajax({
+            url: consulta,
+            type: 'GET',
+            success: function(ans) {
+                $("#contenedor").html(ans);
+                console.log("Succes");
+            },
+            error: function(){
+                $("#contenedor").html("<p> NO ANDUVO WACHO </p>");
+                console.log("Fail");
+            }
+        });
+    });
+});
+
+$(document).ready(function(){
+    $("#estilo2").click(function(){
+        //hacer  consulta Ajax
+        var consulta = "http://localhost:4567/getNoticias2";
+        $("#consulta").html("la consulta es:  "+consulta);
+        /*
+        $.get(consulta, function(answer){
+            //insertar codigo obtenido
+            console.log("consulta recibida");
+            $("#contenedorimagen").html(answer);
+        });
+        */
+        
+        $.ajax({
+            url: consulta,
+            type: 'GET',
+            success: function(ans) {
+                $("#contenedor").html(ans);
+                console.log("Succes");
+            },
+            error: function(){
+                $("#contenedor").html("<p> NO ANDUVO WACHO </p>");
+                console.log("Fail");
+            }
+        });
+    });
+});
+
+$(document).ready(function () {
+
+    $('#sidebarCollapse').on('click', function () {
+        $('#sidebar').toggleClass('active');
+    });
+
+});
