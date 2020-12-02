2d5bc22968d10af95f17c81fceee6b8b144a560a
Luciano <ljcanales1404@gmail.com>
2020-12-02 20:57:05 -0300

    scripts at the end

--- eCommerceLeon_v1/target/classes/public/userScripts.js
+++ eCommerceLeon_v1/target/classes/public/userScripts.js
@@ -3,7 +3,7 @@ function agregarCarrito(id_producto){
 		var url = "http://localhost:4567/addProducto?id_producto=" + id_producto.toString() + "&cant=1";
 
 		//alert(url);
-		
+		//console.log("cantidad"+$(element).parent().find("input").val());
         $.ajax({
             url: url,
             type: 'GET',
@@ -21,16 +21,16 @@ function agregarCarrito(id_producto){
 $(document).ready(function(){
 	var url = "http://localhost:4567/updateCarrito";
 
-        $.ajax({
-            url: url,
-            type: 'GET',
-            success: function(ans) {
-                $("#micarrito").html(ans);
-                console.log("Succes");
-            },
-            error: function(){
-                $("#micarrito").html("<p> Error Ajax addProducto </p>");
-                console.log("Fail");
-            }
-        });	
+    $.ajax({
+        url: url,
+        type: 'GET',
+        success: function(ans) {
+            $("#micarrito").html(ans);
+            console.log("Succes");
+        },
+        error: function(){
+            $("#micarrito").html("<p> Error Ajax addProducto </p>");
+            console.log("Fail");
+        }
+    });
 });
