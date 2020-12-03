ca7333390c920e9563748f4ce49070f3cfd3f358
Luciano <ljcanales1404@gmail.com>
2020-12-03 16:57:58 -0300

    Añadir a carrito, captura de cantidad

--- eCommerceLeon_v1/src/main/resources/public/userScripts.js
+++ eCommerceLeon_v1/src/main/resources/public/userScripts.js
@@ -1,21 +1,23 @@
-function agregarCarrito(id_producto){  
+function agregarCarrito(id_producto,element){  
 
-		var url = "http://localhost:4567/addProducto?id_producto=" + id_producto.toString() + "&cant=1";
 
-		//alert(url);
-		//console.log("cantidad"+$(element).parent().find("input").val());
-        $.ajax({
-            url: url,
-            type: 'GET',
-            success: function(ans) {
-                $("#micarrito").html(ans);
-                console.log("Succes");
-            },
-            error: function(){
-                $("#micarrito").html("<p> Error Ajax addProducto </p>");
-                console.log("Fail");
-            }
-        });
+    var cant = $(element).parent().parent().find("input").val();
+    var url = "http://localhost:4567/addProducto?id_producto=" + id_producto.toString() + "&cant=" + cant;
+
+    //alert(url);
+    //console.log("cantidad : "+$(element).parent().parent().find("input").val());
+    $.ajax({
+        url: url,
+        type: 'GET',
+        success: function(ans) {
+            $("#micarrito").html(ans);
+            console.log("Succes");
+        },
+        error: function(){
+            $("#micarrito").html("<p> Error Ajax addProducto </p>");
+            console.log("Fail");
+        }
+    });
 }
 
 $(document).ready(function(){
