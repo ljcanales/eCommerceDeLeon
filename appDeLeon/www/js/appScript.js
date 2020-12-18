5159afa1f0b782035e3dd83277d6a8d27eb3e515
Luciano <ljcanales1404@gmail.com>
2020-12-18 02:11:59 -0300

    prueba sale mal

--- appDeLeon/www/js/appScript.js
+++ appDeLeon/www/js/appScript.js
@@ -2,4 +2,18 @@ document.addEventListener("deviceready", onDeviceReady, false);
 
 function onDeviceReady() {
     // Now safe to use device APIs
+
+    /* Set the width of the side navigation to 250px */
+  
 }
+
+function openNav() {
+  //document.getElementById("mySidenav").style.width = "250px";
+  $("#mySidenav").css("width", "250px");
+  console.log("ola");
+}
+
+/* Set the width of the side navigation to 0 */
+function closeNav() {
+  document.getElementById("mySidenav").style.width = "0";
+} 
